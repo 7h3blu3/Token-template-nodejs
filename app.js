c93a799098ed88753692b4014384978483425e19
Milen Deyanov <milen.krasimirov.deyanov@gmail.com>
2022-08-03 17:26:01 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const authRoutes = require("./routes/auth")
+const userRoutes = require("./routes/auth")
+
+const MONGODB_URI = 'mongodb+srv://7h3blu3:73XBU9fI69AV8jTk@cluster0.dm69koz.mongodb.net/assessment?retryWrites=true&w=majority'
+
+const app = express();
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database: ");
+    console.log(MONGODB_URI);
+  })
+  .catch((err) => {
+    console.log(err + " Connection to database failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/", authRoutes, userRoutes)
+
+module.exports = app;
